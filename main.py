e790ef3c4395ef3c93eefff301beb17fd217073f
Pietro Domingues <pietro.domingues@quintoandar.com.br>
2022-10-24 23:48:00 -0300

    first classes

---
+++ main.py
@@ -0,0 +1,17 @@
+class Commodity:
+    name
+    description
+
+class Agent:
+    capital
+    commodities
+
+class Transformation:
+    initialAgent
+    finalAgent
+
+class Exchange:
+    primaryAgent
+    secondaryAgent
+    capitalFlow
+    commoditiesFlow
