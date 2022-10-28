b06fc7b19eb6202c106032de69da895e442d5359
Pietro Domingues <pietro.domingues@quintoandar.com.br>
2022-10-28 19:17:26 -0300

    refining

--- agents.py
+++ agents.py
@@ -2,61 +2,6 @@ from entities import Agent, BuyIntent, SellIntent
 from multiset import Multiset as m
 from commodities import food, fertiliser, farmerWork, factoryWork, minerals, minerWork
 
-class Land(Agent):
-    def transform(self):
-        #natural daily production without fertiliser
-        self.commodities += m({food:1})
-
-        #daily production with fertiliser
-        self.convert(
-            m({fertiliser: 1, farmerWork: 1}), 
-            m({food: 10}),
-            greedy = True,
-        )
-
-        self.intents = [
-            BuyIntent({farmerWork}),
-            BuyIntent({fertiliser}),
-            SellIntent({food})
-        ]
-
-class Factory(Agent):
-    def transform(self):
-        self.convert(
-            m({factoryWork: 1, minerals: 5}), 
-            m({fertiliser: 5}),
-            greedy = True
-        )
-
-class Mining(Agent):
-    def transform(self):
-        self.convert(
-            {minerWork}, 
-            m({minerals: 5}),
-            greedy = True
-        )
-
-
-class Market(Agent):
-    def __init__(self, money:float = 0, commodities: m = m({})):
-        self.unmatchedTime = 0 
-        self.buyIntent = BuyIntent({food}, 10)
-        super().__init__(money,commodities)
-        
-    def transform(self):
-        if(self.old.buyIntent.status == 'unmatched'):
-            self.unmatchedTime += 1
-        else:
-            self.unmatchedTime = 0
-    
-        if(self.unmatchedTime == 5):
-            self.buyIntent.money += 1
-            self.unmatchedTime = 0
-
-        self.intents = [
-            self.buyIntent
-        ]
-
 class BaseWorker(Agent):
     def __init__(self, workForce, money:float = 0, commodities: m = m({})):
         super().__init__(money,commodities)
@@ -86,13 +31,72 @@ class Farmer(BaseWorker):
 
         return super().transform()
 
-class Worker(BaseWorker):
-    def __init__(self, money:float = 0, commodities: m = m({})):
-        super().__init__(factoryWork, money, commodities)
+class Land(Agent):
+    def transform(self):
+        #natural daily production without fertiliser
+        self.commodities += m({food:1})
+
+        #daily production with fertiliser
+        self.convert(
+            m({fertiliser: 1, farmerWork: 1}), 
+            m({food: 10}),
+            greedy = True,
+        )
+
+        self.intents = [
+            BuyIntent({farmerWork}),
+            BuyIntent({fertiliser}),
+            SellIntent({food})
+        ]
 
 class Miner(BaseWorker):
     def __init__(self, money:float = 0, commodities: m = m({})):
         super().__init__(minerWork, money, commodities)
 
+    def transform(self):
+        self.intents = [
+            SellIntent({minerWork}),
+            BuyIntent({food}, 5),
+        ]
 
+        return super().transform()
 
+class Mining(Agent):
+    def transform(self):
+        self.convert(
+            {minerWork}, 
+            m({minerals: 5}),
+            greedy = True
+        )
+
+class Worker(BaseWorker):
+    def __init__(self, money:float = 0, commodities: m = m({})):
+        super().__init__(factoryWork, money, commodities)
+
+class Factory(Agent):
+    def transform(self):
+        self.convert(
+            m({factoryWork: 1, minerals: 5}), 
+            m({fertiliser: 5}),
+            greedy = True
+        )
+
+class Market(Agent):
+    def __init__(self, money:float = 0, commodities: m = m({})):
+        self.unmatchedTime = 0 
+        self.buyIntent = BuyIntent({food}, 10)
+        super().__init__(money,commodities)
+        
+    def transform(self):
+        if(self.old.buyIntent.status == 'unmatched'):
+            self.unmatchedTime += 1
+        else:
+            self.unmatchedTime = 0
+    
+        if(self.unmatchedTime == 5):
+            self.buyIntent.money += 1
+            self.unmatchedTime = 0
+
+        self.intents = [
+            self.buyIntent
+        ]
